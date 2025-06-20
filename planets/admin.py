63be1fcfc8c379ef257433afeacee1857d91c931
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-06-19 23:06:34 -0400

    add admin for planet and star models

--- planets/admin.py
+++ planets/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Star, Planet
+
+@admin.register(Star)
+class StarAdmin(admin.ModelAdmin):
+    list_display = ('name', 'mass', 'radius', 'temperature', 'distance')
+    search_fields = ()
+
+@admin.register(Planet)
+class PlanetAdmin(admin.ModelAdmin):
+    list_display = ('name', 'host_star', 'orbital_period', 'radius', 'mass', 'discovered_year')
+    search_fields = ()
