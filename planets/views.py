fbbdb8552118b6e0891d1ddccaa6639b65d6cb91
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-06-26 16:45:03 -0400

    add view to retrieve planet data

--- planets/views.py
+++ planets/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Planet
 
-# Create your views here.
+def planets(request):
+    """
+    A view to display a list of all planets in the database
+    """
+    planets = Planet.objects.select_related('host_star').order_by('name')
+
+    context = {
+        'planets': planets
+    }
+
+    return render(request, 'planets/planets.html', context)
