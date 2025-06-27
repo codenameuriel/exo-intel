ea40247e7e11dad1bc3727a388b82d955d6430fa
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-06-27 19:09:21 -0400

    register api urls

--- planets/urls.py
+++ planets/urls.py
@@ -1,8 +1,15 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
 
 app_name = 'planets'
 
+router = DefaultRouter()
+router.register(r'planets', views.PlanetViewSet, basename='planet')
+router.register(r'starsystems', views.StarSystemViewSet, basename='starsystem')
+router.register(r'stars', views.StarSerialier, basename='star')
+
 urlpatterns = [
     path('', views.planets, name='planets'),
+    path('api/', include(router.urls)),
 ]
