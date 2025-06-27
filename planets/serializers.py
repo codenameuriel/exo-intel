d7af075138466ebcd3ca3ea9f59b658a59b8cf53
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-06-27 19:08:32 -0400

    add data serializers

---
+++ planets/serializers.py
@@ -0,0 +1,59 @@
+from rest_framework import serializers
+from .models import Planet, StarSystem, Star
+
+class PlanetSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Planet model.
+    """
+
+    host_star = serializers.StringRelatedField()
+
+    class Meta:
+        model = Planet
+        fields = [
+            'id',
+            'name',
+            'host_star',
+            'mass_earth',
+            'radius_earth',
+            'orbital_period',
+            'equilibrium_temperature',
+            'discovery'
+        ]
+
+class StarSystemSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the StarSystem model.
+    """
+
+    class Meta:
+        model = StarSystem
+        fields = [
+            'id',
+            'name',
+            'num_stars',
+            'num_planets',
+            'num_moons',
+            'distance',
+            'ra',
+            'dec'
+        ]
+
+class StarSerializer(serializers.ModelSerializer):
+    """
+    Serializer for the Star model.
+    """
+
+    system = serializers.StringRelatedField()
+
+    class Meta:
+        model = Star
+        fields = [
+            'id',
+            'name',
+            'system',
+            'spect_type',
+            'mass',
+            'radius',
+            'temperature'
+        ]
