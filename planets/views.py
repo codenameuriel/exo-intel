26c250c3077d466ae54d5ee9f8ce541e6596b959
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-06-27 19:09:02 -0400

    add viewsets for models

--- planets/views.py
+++ planets/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-from .models import Planet
+from rest_framework import viewsets
+from .models import Planet, StarSystem, Star
+from .serializers import PlanetSerializer, StarSystemSerializer, StarSerializer
+
+class PlanetViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows planets to be viewed or edited.
+    """
+    queryset = Planet.objects.select_related('host_star').order_by('name')
+    serializer_class = PlanetSerializer
+
+class StarSystemViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows star systems to be viewed.
+    """
+    queryset = StarSystem.objects.all().order_by('name')
+    serializer_class = StarSystemSerializer
+
+class StarSerialier(viewsets.ModelViewSet):
+    """
+    API endpoint that allows stars to be viewed.
+    """
+    queryset = Star.objects.select_related('system').order_by('name')
+    serializer_class = StarSerializer
 
 def planets(request):
     """
