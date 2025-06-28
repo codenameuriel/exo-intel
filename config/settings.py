a859c19f1c5e255fcc806db8132b857ff9298f86
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-06-27 21:11:11 -0400

    add celery config

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Celery settings
+CELERY_BROKER_URL = config('CELERY_BROKER_URL', default='redis://localhost:63790')
