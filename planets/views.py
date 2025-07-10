a19cb5a8e46743de2d1608342790bbc2d5bd25a1
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-07-10 16:04:28 -0400

    format with black and configure auth and permission to DRF api views

--- planets/views.py
+++ planets/views.py
@@ -1,51 +1,84 @@
 from django.shortcuts import render
 from rest_framework import viewsets, filters
 from django_filters.rest_framework import DjangoFilterBackend
-# from django.db.models import F, Case, When, Value, FloatField, IntegerField, Q, ExpressionWrapper
-# from django.db.models.functions import Abs, Round
 from .models import Planet, StarSystem, Star
 from .serializers import PlanetSerializer, StarSystemSerializer, StarSerializer
 from .filters import PlanetFilter
+from api_keys.authentication import APIKeyAuthentication
+from api_keys.permissions import IsAuthenticatedOrPublic
+from rest_framework.authentication import SessionAuthentication
+
 
 class PlanetViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows planets to be viewed or edited.
+    Read-only API endpoint for planets.
+    This is the "freemium" resource.
     """
-    queryset = Planet.objects.with_habitability().select_related('host_star', 'discovery').order_by('name')
+
+    authentication_classes = [APIKeyAuthentication]
+    permission_classes = [IsAuthenticatedOrPublic]
+    is_public_resource = True
+
+    queryset = (
+        Planet.objects.with_habitability()
+        .select_related("host_star", "discovery")
+        .order_by("name")
+    )
     serializer_class = PlanetSerializer
-    serializer_class = PlanetSerializer
-    filter_backends = [filters.SearchFilter, DjangoFilterBackend, filters.OrderingFilter]
-    search_fields = ['name', 'host_star__name']
+    filter_backends = [
+        filters.SearchFilter,
+        DjangoFilterBackend,
+        filters.OrderingFilter,
+    ]
+    search_fields = ["name", "host_star__name"]
     filterset_class = PlanetFilter
-    ordering_fields = ['habitability_score', 'mass_earth', 'radius_earth', 'orbital_period']
+    ordering_fields = [
+        "habitability_score",
+        "mass_earth",
+        "radius_earth",
+        "orbital_period",
+    ]
+
 
 class StarSystemViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows star systems to be viewed.
+    Read-only API endpoint for star systems.
+    This a private resource.
     """
-    queryset = StarSystem.objects.all().order_by('name')
+
+    authentication_classes = [APIKeyAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrPublic]
+    is_public_resource = False
+
+    queryset = StarSystem.objects.all().order_by("name")
     serializer_class = StarSystemSerializer
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-    search_fields = ['name']
-    filterset_fields = ['num_stars', 'num_planets']
+    search_fields = ["name"]
+    filterset_fields = ["num_stars", "num_planets"]
+
 
 class StarViewSet(viewsets.ReadOnlyModelViewSet):
     """
-    API endpoint that allows stars to be viewed.
+    Read-only API endpoint for stars.
+    This is a private resource.
     """
-    queryset = Star.objects.select_related('system').order_by('name')
+
+    authentication_classes = [APIKeyAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrPublic]
+    is_public_resource = False
+
+    queryset = Star.objects.select_related("system").order_by("name")
     serializer_class = StarSerializer
     filter_backends = [filters.SearchFilter, DjangoFilterBackend]
-    search_fields = ['name', 'system__name']
-    filterset_fields = ['spect_type', 'system__name']
+    search_fields = ["name", "system__name"]
+    filterset_fields = ["spect_type", "system__name"]
+
 
 def planets(request):
     """
     A view to display a list of all planets in the database
     """
-    planets = Planet.objects.select_related('host_star').order_by('name')
-    context = {
-        'planets': planets
-    }
+    planets = Planet.objects.select_related("host_star").order_by("name")
+    context = {"planets": planets}
 
-    return render(request, 'planets/planets.html', context)
+    return render(request, "planets/planets.html", context)
