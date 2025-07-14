da6530c4374be0a7561da5e333326a25990b4bf5
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-07-14 17:18:34 -0400

    add graphql endpoint

--- planets/urls.py
+++ planets/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from django.views.decorators.csrf import csrf_exempt
 
 app_name = "planets"
 
@@ -13,4 +14,5 @@ router.register(r"stars", views.StarViewSet, basename="star")
 urlpatterns = [
     path("", views.planets, name="planets"),
     path("api/", include(router.urls)),
+    path("graphql/", csrf_exempt(views.PrivateGraphQLView.as_view()), name="graphql"),
 ]
