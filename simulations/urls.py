96c392382c20ca03b5c91b84f27f8ced35baeb47
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-07-22 15:03:08 -0400

    add new endpoint

--- simulations/urls.py
+++ simulations/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path
-from .views import TravelTimeSimulationView
+from .views import TravelTimeSimulationView, SeasonalTempsSimulationView
 
 app_name = "simulations"
 
 urlpatterns = [
-        path(
-            "travel-time/",
-            TravelTimeSimulationView.as_view(),
-            name="simulation-travel-time",
+    path(
+        "travel-time/",
+        TravelTimeSimulationView.as_view(),
+        name="simulation-travel-time",
+    ),
+    path(
+        "seasonal-temps/",
+        SeasonalTempsSimulationView.as_view(),
+        name="simulation-seasonal-temps",
     ),
 ]
