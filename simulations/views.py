87c3fc1829e31ed07d962b6f1c25408e5caa662f
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-07-22 15:02:48 -0400

    add new simulation view and refactor input validation

--- simulations/views.py
+++ simulations/views.py
@@ -4,8 +4,12 @@ from rest_framework.response import Response
 from rest_framework.authentication import SessionAuthentication
 from api_keys.authentication import APIKeyAuthentication
 from api_keys.permissions import IsAuthenticatedOrPublic
-from planets.models import StarSystem
-from tasks.tasks import travel_time_simulation_task
+from simulations.serializers import (
+    TravelTimeInputSerializer,
+    SeasonalTempInputSerializer,
+)
+from tasks.tasks import travel_time_simulation_task, seasonal_temps_simulation_task
+
 
 class TravelTimeSimulationView(APIView):
     """
@@ -20,39 +24,44 @@ class TravelTimeSimulationView(APIView):
         """
         Handles POST request to run the simulation.
         """
-        system_id = request.data.get("star_system_id")
-        speed_percentage = request.data.get("speed_percentage")
+        serializer = TravelTimeInputSerializer(data=request.data)
 
-        if not system_id or not speed_percentage:
-            return Response(
-                {"error": "Both 'star_system_id' and 'speed_percentage' are required."},
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        serializer.is_valid(raise_exception=True)
 
-        try:
-            system_id = int(system_id)
-            speed_percentage = float(speed_percentage)
-            if not (0 < speed_percentage <= 100):
-                raise ValueError()
-        except (ValueError, TypeError):
-            return Response(
-                {
-                    "error": "'star_system_id' must be an integer and 'speed_percentage' must be a valid number between 1 and 100."
-                },
-                status=status.HTTP_400_BAD_REQUEST,
-            )
+        task = travel_time_simulation_task.delay(**serializer.validated_data)
 
-        if not StarSystem.objects.filter(id=system_id).exists():
-            return Response(
-                {"error": f"StarSystem with id {system_id} not found."},
-                status=status.HTTP_404_NOT_FOUND,
-            )
+        return Response(
+            {
+                "message": "Simulation task has been started.",
+                "task_id": task.id,
+            },
+            status=status.HTTP_202_ACCEPTED,
+        )
 
-        task = travel_time_simulation_task.delay(system_id, speed_percentage)
 
-        response_data = {
-            "message": "Simulation task has been started.",
-            "task_id": task.id,
-        }
+class SeasonalTempsSimulationView(APIView):
+    """
+    An "action" API endpoint to calculate the seasonal temperatures for a planet.
+    """
 
-        return Response(response_data, status=status.HTTP_202_ACCEPTED)
+    authentication_classes = [APIKeyAuthentication, SessionAuthentication]
+    permission_classes = [IsAuthenticatedOrPublic]
+    is_public_resource = False
+
+    def post(self, request, *args, **kwargs):
+        """
+        Handles POST request to run the simulation.
+        """
+        serializer = SeasonalTempInputSerializer(data=request.data)
+
+        serializer.is_valid(raise_exception=True)
+
+        task = seasonal_temps_simulation_task.delay(**serializer.validated_data)
+
+        return Response(
+            {
+                "message": "Simulation task has been started.",
+                "task_id": task.id,
+            },
+            status=status.HTTP_202_ACCEPTED,
+        )
