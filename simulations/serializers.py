1127d85e2174a713ab567ba8702a7b36cc12e2f0
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-07-22 21:35:04 -0400

    refactor serializers

--- simulations/serializers.py
+++ simulations/serializers.py
@@ -1,5 +1,4 @@
 from rest_framework import serializers
-from planets.models import StarSystem, Planet
 
 
 class TravelTimeInputSerializer(serializers.Serializer):
@@ -10,10 +9,6 @@ class TravelTimeInputSerializer(serializers.Serializer):
     star_system_id = serializers.IntegerField()
     speed_percentage = serializers.FloatField(min_value=1, max_value=100)
 
-    def validate_star_system_id(self, value):
-        if not StarSystem.objects.filter(id=value).exists():
-            raise serializers.ValidationError(f"StarSystem with id {value} not found.")
-
 
 class SeasonalTempInputSerializer(serializers.Serializer):
     """
@@ -22,6 +17,10 @@ class SeasonalTempInputSerializer(serializers.Serializer):
 
     planet_id = serializers.IntegerField()
 
-    def validate_planet_id(self, value):
-        if not Planet.objects.filter(id=value).exists():
-            raise serializers.ValidationError(f"Planet with id {value} not found.")
+
+class TidalLockingInputSerializer(serializers.Serializer):
+    """
+    A class to serialize input data for validation
+    """
+
+    planet_id = serializers.IntegerField()
