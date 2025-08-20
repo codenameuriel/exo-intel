22f57b51a6594b8ed8761619b287f575944a31b1
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-08-19 21:46:25 -0400

    redirect home page to portal login

--- config/urls.py
+++ config/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.views.generic import RedirectView
 
 urlpatterns = [
+    path("", RedirectView.as_view(pattern_name="portal:login"), name="home"),
     path("admin/", admin.site.urls),
     path("planets/", include("planets.urls")),
     path("portal/", include("portal.urls")),
