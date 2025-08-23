794c9f363d9a30d69d39ef494d73845d349f97c9
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-08-22 21:03:07 -0400

    refactor signup page to reduce help texts

--- portal/forms.py
+++ portal/forms.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model, password_validation
 
 
 class SignupForm(UserCreationForm):
@@ -10,8 +10,35 @@ class SignupForm(UserCreationForm):
     """
 
     class Meta(UserCreationForm.Meta):
-        model = User
+        model = get_user_model()
+        # model based fields
         fields = (
             "username",
             "email",
         )
+        help_texts = {
+            f: "" for f in fields
+        }
+        labels = {
+            "username": "Username",
+            "email": "Email",
+            "password1": "Password",
+            "password2": "Confirm Password",
+        }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        # remove dynamic form derived fields
+        for name in ("password1", "password2"):
+            self.fields[name].help_text = ""
+
+        self.fields["email"].required = True
+
+    def clean_password1(self):
+        password = self.cleaned_data.get("password1")
+        password_validation.validate_password(password, self.instance)
+        return password
+
+    @property
+    def password_help_list(self):
+        return password_validation.password_validators_help_texts()
