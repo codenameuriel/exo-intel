1a9e8ac2533a809f3e57afa0c2580afb6eaf4d83
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-08-26 09:03:14 -0400

    add docker build time defaults to ignore .env files within containers

--- config/settings/production.py
+++ config/settings/production.py
@@ -38,19 +38,19 @@ LOGGING = {
 
 # enable fingerprinting, hashed filenames for improved caching and smaller asset files
 STORAGES = {
-  "staticfiles": {
-    "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
-  }
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage"
+    }
 }
 
-SECRET_KEY = env("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY", default="__build_placeholder__")
 DEBUG = env("DEBUG")
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["localhost", "127.0.0.1"])
 DJANGO_ADMIN_URL = env("DJANGO_ADMIN_URL", default="admin/")
 
 DATABASES = {
     "default": dj_database_url.config(
-        default=env("DATABASE_URL"),
+        default=env("DATABASE_URL", default="sqlite:///buildtime.sqlite3"),
         # keep connection alive for 10 minutes
         conn_max_age=600,
     )
@@ -60,10 +60,10 @@ MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-CELERY_BROKER_URL = env("CELERY_BROKER_URL")
+CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://redis:6379/0")
 
 # configure where to store the status and results of completed tasks
 # use Redis database #1
-CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND")
+CELERY_RESULT_BACKEND = env("CELERY_RESULT_BACKEND", default="redis://redis:6379/1")
 
 NASA_TAP_BASE_URL = "https://exoplanetarchive.ipac.caltech.edu/TAP/sync"
