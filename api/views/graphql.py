fb157ed93a747c1898451f0bb449ad17ff73af91
Uriel Rodriguez <codenameuriel28@gmail.com>
2025-08-30 15:49:14 -0400

    update gql endpoint swagger doc

--- api/views/graphql.py
+++ api/views/graphql.py
@@ -1,10 +1,12 @@
+from drf_spectacular.utils import extend_schema, OpenApiExample, OpenApiResponse, inline_serializer
 from graphene_django.views import GraphQLView
+from rest_framework import serializers
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.views import APIView
 
+from api.parsers import GraphQLParser
 from api_keys.authentication import APIKeyAuthentication
 from api_keys.permissions import IsAuthenticatedOrPublic
-from api.parsers import GraphQLParser
 
 
 class PrivateGraphQLView(APIView):
@@ -20,6 +22,12 @@ class PrivateGraphQLView(APIView):
     # GraphQLView, which solves the "cannot access body after reading" error.
     parser_classes = [GraphQLParser]
 
+    @extend_schema(
+        description=(
+                "Access this endpoint in a web browser to open the interactive GraphiQL IDE. "
+                "This is the primary tool for exploring and testing the GraphQL schema."
+        )
+    )
     def get(self, request, *args, **kwargs):
         """
         Handles browser GET requests which are used to render the GraphiQL interface
@@ -27,6 +35,50 @@ class PrivateGraphQLView(APIView):
         self.check_permissions(request)
         return GraphQLView.as_view(graphiql=True)(request, *args, **kwargs)
 
+    @extend_schema(
+        description="Send GraphQL queries to this endpoint via a POST request.",
+        request=inline_serializer(
+            name='GraphQLRequest',
+            fields={
+                'query': serializers.CharField(help_text='The GraphQL query string.'),
+            },
+        ),
+        responses={
+            200: OpenApiResponse(
+                response=inline_serializer(
+                    name='GraphQLResponse',
+                    fields={
+                        'data': serializers.DictField(help_text='The GraphQL result data.'),
+                        'errors': serializers.ListField(
+                            child=serializers.DictField(), help_text='Any errors encountered.'
+                        ),
+                    },
+                ),
+            )
+        },
+        examples=[
+            OpenApiExample(
+                'Sample request',
+                value={"query": "query { allStars(first: 3) { edges { node { name } } } }"},
+                request_only=True,
+            ),
+            OpenApiExample(
+                'Sample response',
+                value={
+                    "data": {
+                        "allStars": {
+                            "edges": [
+                                {"node": {"name": "Kepler-27"}},
+                                {"node": {"name": "Kepler-633"}},
+                                {"node": {"name": "Kepler-1063"}},
+                            ]
+                        }
+                    }
+                },
+                response_only=True,
+            ),
+        ],
+    )
     def post(self, request, *args, **kwargs):
         """
         Handles programmatic client POST requests which are used to simply return JSON.
